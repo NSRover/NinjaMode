2d2b2e72cb5c82cd36204323ec721ea914299a45
nirbhayg <nirbhay.agarwal@gmail.com>
2014-10-10 10:39:15 +0530

    Menubar app to toggle Dark mode

--- NinjaMode/AppDelegate.m
+++ NinjaMode/AppDelegate.m
@@ -11,12 +11,59 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (assign, nonatomic) BOOL darkModeOn;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    _statusItem.image = [NSImage imageNamed:@"switchIcon.png"];
+    [_statusItem.image setTemplate:YES];
+
+    _statusItem.highlightMode = NO;
+    _statusItem.toolTip = @"command-click to quit";
+    
+    [_statusItem setAction:@selector(itemClicked:)];
+    [self refreshDarkMode];
+}
+
+- (void)refreshDarkMode {
+    NSString * value = (__bridge NSString *)(CFPreferencesCopyValue((CFStringRef)@"AppleInterfaceStyle", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost));
+    if ([value isEqualToString:@"Dark"]) {
+        self.darkModeOn = YES;
+    }
+    else {
+        self.darkModeOn = NO;
+    }
+}
+
+- (void)itemClicked:(id)sender {
+
+    NSEvent *event = [NSApp currentEvent];
+    if([event modifierFlags] & NSControlKeyMask) {
+        [[NSApplication sharedApplication] terminate:self];
+        return;
+    }
+    
+    _darkModeOn = !_darkModeOn;
+    
+    //Change pref
+    if (_darkModeOn) {
+        CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", @"Dark", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    }
+    else {
+        CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", NULL, kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    }
+    
+    //update listeners
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDistributedCenter(), (CFStringRef)@"AppleInterfaceThemeChangedNotification", NULL, NULL, YES);
+    });
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
